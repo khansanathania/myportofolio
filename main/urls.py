8333785a089ca49509afbbf0a7e13bffa6d48ee9
khansanathania <khansa.nathania@ui.ac.id>
2026-09-19 10:08:37 +0700

    feat: add update_achievement route at main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ from main.views import (
     show_achievement,
     show_achievement_detail,
     create_achievement,
+    update_achievement,
     get_achievements_json,
     delete_achievement,
 )
@@ -19,6 +20,7 @@ urlpatterns = [
     path("achievement/", show_achievement, name="show_achievement"),
     path("achievement/<int:id>/", show_achievement_detail, name="show_achievement_detail"),
     path("achievement/add/", create_achievement, name="create_achievement"),
+    path("achievement/<int:id>/edit/", update_achievement, name="update_achievement"),
     path("api/achievements/", get_achievements_json, name="get_achievements_json"),
     path("achievement/<int:achievement_id>/delete/", delete_achievement, name="delete_achievement"),
 ]
